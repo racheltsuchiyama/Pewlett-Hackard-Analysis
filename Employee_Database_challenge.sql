fa4e750dce2ab3d4e85b057ac9c01cb9116d94e9
racheltsuchiyama <83552696+racheltsuchiyama@users.noreply.github.com>
2021-06-12 20:56:54 -0700

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,1 +1,29 @@
+-- Create retirement titles table (includes duplicates)
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, t.title DESC;
 
+-- Select the most recent employee title
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Retrieve number of employees by most recent job title 
+SELECT COUNT(title) as count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
