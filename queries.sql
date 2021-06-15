93796016abe175d252dd98d3ebc120b55b33a8df
racheltsuchiyama <83552696+racheltsuchiyama@users.noreply.github.com>
2021-06-14 19:05:25 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -37,26 +37,3 @@ FROM mentorship_elgibility
 GROUP BY title
 ORDER BY count DESC;
 
--- Create new mentorship eligibility table
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.birth_date,
-	de.from_date,
-	de.to_date,
-	t.title
-INTO new_mentorship
-FROM employees as e
-JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE de.to_date = ('1999-01-01')
-AND (e.birth_date BETWEEN '1964-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
-
--- Retrieve number of employees for adjusted mentorship program
-SELECT COUNT(title) as count, title
-FROM new_mentorship
-GROUP BY title
-ORDER BY count DESC;
